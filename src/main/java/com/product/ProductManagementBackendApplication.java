ff852a30fcdbd016fe0cf8c018e6fbe88765fd9c
FATHIMA FARSEENA VS <19mcs50@meaec.edu.in>
2024-01-04 10:23:21 +0530

    Created Product management system backend project

---
+++ src/main/java/com/product/ProductManagementBackendApplication.java
@@ -0,0 +1,14 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/product/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.product.service;
+
+import java.util.List;
+
+import com.product.model.Product;
+
+public interface ProductService {
+
+	public Product saveProduct(Product product);
+
+	public List<Product> getAllProduct();
+
+	public Product getProductById(Integer id);
+
+	public String deleteProduct(Integer id);
+
+	public Product editProduct(Product product,Integer id);
+	
+}
