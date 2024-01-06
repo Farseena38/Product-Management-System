2e1dea017d3f29c56d2953e6d19474355aa3a869
FATHIMA FARSEENA VS <19mcs50@meaec.edu.in>
2024-01-06 21:08:55 +0530

    Product management system project successfully completed

--- Product_Management_UI/product_ui/src/component/Navbar.js
+++ Product_Management_UI/product_ui/src/component/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Reat App</a>
+                    <a className="navbar-brand" href="#"> Product Management System</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -14,9 +14,9 @@ const Navbar = () => {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             
                             <li className="nav-item">
-                                <Link to="/" className="nav-link active" aria-current="page" href="#">Home</Link>
+                                <Link to="/" className="nav-link active" aria-current="page" href="#">Home<hover></hover></Link>
                             </li>
-
+                            
                             <li className="nav-item">
                                 <Link  to="addProduct" className="nav-link active" aria-current="page" href="#">Add Product</Link>
                             </li>
